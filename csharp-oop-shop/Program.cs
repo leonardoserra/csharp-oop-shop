07537fed3f85372193f09b0eb0bb374be2ac1b2c
leonardoserra <serra.leonardo92@gmail.com>
2023-09-08 17:00:15 +0200

    Bonus completato con stampa info prodotto

--- csharp-oop-shop/Program.cs
+++ csharp-oop-shop/Program.cs
@@ -1,8 +1,42 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using csharp_oop_shop;
 
-Product[] products =
+internal class Program
 {
+    private static void Main(string[] args)
+    {
+#region ESERCIZIO BASE
+        Console.WriteLine("---------------------ESERCIZIO BASE----------------------");
+        Console.WriteLine();
+
+
+        Product ball = new Product("Palla da Basket", "Una palla per giocare a basket", 23);
+        Console.Write("Codice Prodotto: ");
+        Console.WriteLine(ball.FormatIdCode());
+        Console.Write("Nome Prodotto: ");
+        Console.WriteLine(ball.Name);
+        Console.Write("Descrizione Prodotto: ");
+        Console.WriteLine(ball.Description);
+        Console.Write("Prezzo senza iva Prodotto: ");
+        Console.WriteLine(ball.Price);
+        Console.Write("Tassazione Prodotto: ");
+        Console.WriteLine(ball.VAT);
+        ball.PrintSerialName();
+        ball.GetTaxedPrice();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+
+
+        #endregion
+
+#region BONUS
+        Console.WriteLine("----------------BONUS---------------------");
+        Console.WriteLine();
+
+
+        Product[] products =
+        {
     new Product(
         "Longboard",
         "Progettato per fare dei giri e pump track. La sua costruzione riproduce le sensazioni del surf sull'asfalto. Ideale per persone alte più di 1,70 m.",
@@ -29,34 +63,26 @@ Product[] products =
         445
         )
 };
-Console.WriteLine("Benvenuto alla decathlon! Scegli fra la nostra vastissima gamma di prodotti e ti stamperemo le sue caratteristiche!");
-for (int i = 0; i< products.Length;i++ )
-{
-    Console.Write($"{i + 1}: {products[i].Name}");
-    Console.WriteLine();
+        Console.WriteLine("Benvenuto alla decathlon! Scegli fra la nostra vastissima gamma di prodotti e ti stamperemo le sue caratteristiche!");
+        Console.WriteLine();
+
+        for (int i = 0; i < products.Length; i++)
+        {
+            Console.Write($"{i + 1}: {products[i].Name}");
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+        Console.Write("Digita il numero di un prodotto: ");
+
+        int choice = int.Parse(Console.ReadLine());
+        if(choice <= 0||choice > products.Length) 
+            Console.WriteLine("Articolo non trovato.");
+        else
+            products[choice-1].PrintProductInfo(products[choice-1]);
+            
+        
+    }
+#endregion
 }
 
-
-while(int.TryParse(Console.ReadLine(), out int choice)==false){
-    if(choice < 0 || choice > products.Length-1) {
-        continue;
-    }
-    else
-    {
-        products[choice - 1].PrintProductInfo(products[choice - 1]);
-    }
-}
-
-/*Product ball = new Product("Palla da Basket", "Una palla per giocare a basket", 23);
-Console.Write("Codice Prodotto: ");
-Console.WriteLine(ball.FormatIdCode());
-Console.Write("Nome Prodotto: ");
-Console.WriteLine(ball.Name);
-Console.Write("Descrizione Prodotto: ");
-Console.WriteLine(ball.Description);
-Console.Write("Prezzo senza iva Prodotto: ");
-Console.WriteLine(ball.Price);
-Console.Write("Tassazione Prodotto: ");
-Console.WriteLine(ball.VAT);
-ball.PrintSerialName();
-ball.GetTaxedPrice();*/
